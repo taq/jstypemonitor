19ca7ba373619c2b64477817e8875e142d4ecaf6
Eustaquio Rangel <taq@eustaquiorangel.com>
2018-02-04 10:34:12 -0200

    First code.

--- README.md
+++ README.md
@@ -8,3 +8,31 @@ the minimum length.
 
 It's vanilla JavaScript, we don't need any framework to make it works and it can
 be used with all the frameworks.
+
+## Install
+
+Just drop the files from the `src` dir on your code.
+
+## Usage
+
+Like: 
+
+```javascript
+options = { 
+   element: 'journal_entry_creditable_name', 
+   minimumLength: 3, 
+   interval: 250, 
+   callback: function(event) {
+      console.log('element value: ' + event.target.value);
+   }
+}
+(new Bluefish.JSTypeMonitor(options)).fire();
+```
+
+Where:
+
+- **element** can be a DOM element or a string with the element id
+- **minimumLength** is the minimum element value length to fire the callback function
+- **interval** is how many milliseconds will be discarded to get a new value
+- **calllback** is the function to be called after getting the element value
+
---
+++ src/jstypemonitor.js
@@ -0,0 +1,81 @@
+var Bluefish = typeof Bluefish === "undefined" ? {} : Bluefish;
+
+Bluefish.JSTypeMonitor = function(options) {
+   var self = this;
+
+   this.element       = options.element;
+   this.minimumLength = options.minimumLength;
+   this.callback      = options.callback;
+   this.interval      = options.interval;
+   this.lastChange    = new Date();
+
+   /**
+    * Fire watching
+    */
+   this.fire = function() {
+      this.elementRef().onkeyup = this.changed;
+   };
+
+   /**
+    * Element changed
+    */
+   this.changed = function(event) {
+      if (!self.callback) {
+         return;
+      }
+
+      if (!self.checkInterval()) {
+         return;
+      }
+
+      var value = self.elementRef().value;
+      var min   = self.minimum();
+   
+      if (min > 0 && value.length < min) {
+         return;
+      }
+
+      self.callback(event);
+   };
+
+   /**
+    * Return the minimum length, as an integer
+    */
+   this.minimum = function() {
+      var min = parseInt(this.minimumLength);
+      if (isNaN(min)) {
+         return 0;
+      }
+      return min;
+   };
+
+   /**
+    * Return element reference
+    */
+   this.elementRef = function() {
+      if (typeof(this.element) == 'object') {
+         return this.element;
+      }
+      return document.querySelector('#' + this.element);
+   };
+
+   /**
+    * Check interval, in milliseconds
+    */
+   this.checkInterval = function() {
+      var num = parseInt(this.interval);
+
+      if (isNaN(num)) {
+         return false;
+      }
+
+      var diff = (new Date()) - this.lastChange;
+
+      if (diff < this.interval) {
+         return false;
+      }
+
+      this.lastChange = new Date();
+      return true;
+   };
+};
